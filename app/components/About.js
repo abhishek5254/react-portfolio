2c61d0b3679cbfd14b2acb2043c2284d0792b992
pjvalentini <pj.valentini@gmail.com>
2018-01-27 19:58:35 -0500

    adding to the home page

--- app/components/About.js
+++ app/components/About.js
@@ -14,7 +14,7 @@ export default class About extends React.Component {
           <h1 className="about-me-header">About Me:</h1>
           <h2 className="about-main-text">Hi, I am PJ Valentini. This is my design and web development portfolio. I am currently a student of web development, and I am looking for the opportunity to create high color and vibrant designs for websites, applications and graphic design projects catering to all kinds of companies looking to expand their brand. I am a professional photographer and video editor motivated by strong aesthetics to work on creating your vision.</h2><br></br>
           <p className="twitter-blurb">If you'd like to follow me on Twitter, my username is
-          <a class="anchor" href="http://twitter.com/pjdev11">@pjdev11</a>.</p>
+          <a class="anchor" href="http://twitter.com/pjdev11"> @pjdev11</a>.</p>
         <Footer/>
       </div>
     );
--- app/components/Home.js
+++ app/components/Home.js
@@ -11,7 +11,45 @@ export default class Home extends React.Component {
     return (
       <div>
         <Header/>
-        <h1 className="home-header">Welcome to my Site</h1>
+        <h1 className="text-center" id="home-header">Welcome to my Site</h1>
+        <ul id="gallery">
+            <li>
+              <a href="https://github.com/pjvalentini/Lake-Tahoe-Website">
+                <img src="./images/image 1.jpg" alt=""/>
+                <p>Lake Tahoe Tourist Site.</p>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.pjvalentini.com/">
+                <img src="./images/pjvweb.jpg" alt=""/>
+                <p>Photography site.</p>
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/pjvalentini/Dice-App-Version-2.0">
+                <img src="./images/diceapp.png" alt=""/>
+                <p>Virtual Dice App.</p>
+              </a>
+            </li>
+            <li>
+              <a href="img/image 3.jpg">
+                <img src="./images/image 3.jpg" alt=""/>
+                <p>Altar Group Solutions Site.</p>
+              </a>
+            </li>
+            <li>
+              <a href="img/image 5.jpg">
+                <img src="./images/image 5.jpg" alt=""/>
+                <p>3M Innovations Website.</p>
+              </a>
+            </li>
+            <li>
+              <a href="img/image 6.jpg">
+                <img src="./images/image 6.jpg" alt=""/>
+                <p>Site made with Wordpress.</p>
+              </a>
+            </li>
+          </ul>
         <Footer/>
       </div>
     )
